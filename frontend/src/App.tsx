4c6e116dc93b14afc417227f01fe6d01787c5531
Aether Jan <aether@Aethers-MacBook-Pro.local>
2026-01-16 16:58:32 +0800

    Revert "code inteview online"
    This reverts commit cdd3d56c88a296bc419c8a7d5348615e42054a38.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import { ConfigProvider, App as AntdApp } from 'antd';
+import { ConfigProvider } from 'antd';
 import HomePage from './home';
 import SetupPage from './setup';
 import InterviewPage from './interview';
 import ReportPage from './report';
 import QuestionBankPage from './question-bank';
-import CodingPage from './coding';
 
 function App() {
   return (
@@ -16,16 +15,13 @@ function App() {
         },
       }}
     >
-      <AntdApp>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/setup" element={<SetupPage />} />
-          <Route path="/interview" element={<InterviewPage />} />
-          <Route path="/report" element={<ReportPage />} />
-          <Route path="/question-bank" element={<QuestionBankPage />} />
-          <Route path="/coding" element={<CodingPage />} />
-        </Routes>
-      </AntdApp>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/setup" element={<SetupPage />} />
+        <Route path="/interview" element={<InterviewPage />} />
+        <Route path="/report" element={<ReportPage />} />
+        <Route path="/question-bank" element={<QuestionBankPage />} />
+      </Routes>
     </ConfigProvider>
   );
 }
